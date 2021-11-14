3c0b743206a65e9b5391adb12ab26e6ccdc9d3ca
AnneAbhiram <63452476+AnneAbhiram@users.noreply.github.com>
2021-11-14 18:25:50 +0530

    Update UV_may12c.ino

--- UV_may12c.ino
+++ UV_may12c.ino
@@ -38,11 +38,3 @@ void loop()
   }
   delay(3000);
 }
-
-
-
-
-
-/*
-   My project is a UV intensity detector for skin protection.
-   The way it works is it gives the output based on the presence of UV light. The arduino determines the UV intensity based on the input voltage from the sensor and displays if protection is needed for the skin.
